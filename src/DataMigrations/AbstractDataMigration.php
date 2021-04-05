4a4dbaaba2b3ec9e475797f448f202ebb8e50a77
Christian Kaal <christiankaal@macbook-pro-van-christian.local>
2021-04-05 23:10:05 +0200

    WIP data migration implementation

--- src/DataMigrations/AbstractDataMigration.php
+++ src/DataMigrations/AbstractDataMigration.php
@@ -6,5 +6,30 @@ use CrixuAMG\LaravelDataMigrations\Contracts\DataMigration;
 
 abstract class AbstractDataMigration implements DataMigration
 {
+    abstract function migrate();
 
+    function onFail(\Exception $exception)
+    {
+        // TODO: Implement onFail if required
+    }
+
+    function onSuccess()
+    {
+        // TODO: Implement onSuccess if required
+    }
+
+    public function handle()
+    {
+        try {
+            $this->migrate();
+
+            $this->onSuccess();
+
+            $this->create([
+                'name' => get_called_class(),
+            ]);
+        } catch (\Exception $exception) {
+            $this->onFail($exception);
+        }
+    }
 }
